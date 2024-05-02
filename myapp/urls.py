bc7aa2d07bbd3bdc1c93fc284b5471060f07bba4
nahueldev <nabudev01@gmail.com>
2024-05-02 19:59:13 -0300

    trabajando con parametros url, cree un username para concatenarlo en la vista deseada..

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bienvenida/', views.bienvenida),
-    path('despedida/', views.saludo),
+    #path('bienvenida/', views.bienvenida),
+    path('saludo/<str:username>', views.saludo),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 
-def bienvenida(request):
-    return HttpResponse ('Hola maquina')
+#def bienvenida(request):
+#    return HttpResponse ('Hola maquina')
 
-def saludo(request):
-    return HttpResponse ('nos vemos maquina')
+def saludo(request, username):
+    return HttpResponse ('Hola %s' % username)
 
 
