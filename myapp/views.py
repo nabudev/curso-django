9b509a0e216ef0f83e2d217aa2959407521a1226
nahueldev <nabudev01@gmail.com>
2024-05-02 01:55:59 -0300

    creando las primeras vistas: bienvenida y despedida

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def bienvenida(request):
+    return HttpResponse ('Hola maquina')
+
+def saludo(request):
+    return HttpResponse ('nos vemos maquina')
+
+
