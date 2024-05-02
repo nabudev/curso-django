92cd40bae106b0f4464108ca4c30f7917c847fcc
nahueldev <nabudev01@gmail.com>
2024-05-02 02:49:40 -0300

    modelos y bases de datos

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name= models.CharField(max_length=200)
+    
+    
+
+#creando una tabla relacionada con project    
+class Tasks(models.Model):
+    title= models.CharField(max_length=200)
+    description=models.TextField()
+    #clave foranea, entre parentesis el nombre de la tabla a la que pertenece y seguido de la coma la config de como se modificara si se eliminan atributos de la tabla project
+    project=models.ForeignKey(Project, on_delete=models.CASCADE)
